f6519dfec9b1a9ff9a9f4abc38aeca952e3202af
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-04 12:31:29 -0700

    fixed datd and updated sql info

--- anime.data.js
+++ anime.data.js
@@ -5,7 +5,7 @@ const anime = [
       type: 'Adventure',
       url: 'https://en.wikipedia.org/wiki/JoJo%27s_Bizarre_Adventure#/media/File:JoJo_no_Kimyou_na_Bouken_cover_-_vol1.jpg',
       year: 1987,
-      stillRunning: 'yes',
+      stillRunning: 'TRUE',
     },
      {
         id: '2',
@@ -13,31 +13,24 @@ const anime = [
         type: 'Magical Girl',
         url: 'https://en.wikipedia.org/wiki/Sailor_Moon#/media/File:SMVolume1.jpg',
         year: 1991,
-        stillRunning: 'no',
+        stillRunning: 'FALSE',
      },
+     
      {
         id: '3',
-        name: 'Sailor Moon',
-        type: 'Magical Girl',
-        url: 'https://en.wikipedia.org/wiki/Sailor_Moon#/media/File:SMVolume1.jpg',
-        year: 1991,
-        stillRunning: 'no',
-     },
-     {
-        id: '4',
         name: 'Macross Frontier',
         type: 'Space Opera',
         url: 'https://en.wikipedia.org/wiki/File:MF_DVD_cover.jpg',
         year: 2007,
-        stillRunning: 'no',
+        stillRunning: 'FALSE',
      },
      {
-        id: '5',
+        id: '4',
         name: 'Dragon Ball',
         type: 'Action',
         url: 'https://en.wikipedia.org/wiki/Dragon_Ball_(TV_series)#/media/File:Dragon_Ball_anime_logo.png',
         year: 1986 ,
-        stillRunning: 'no',
+        stillRunning: 'FALSE',
      },
     ];
 
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS anime;
+
+CREATE TABLE anime (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  type VARCHAR NOT NULL,
+  url VARCHAR NOT NULL,
+  year INT NOT NULL,
+  stillRunning BOOLEAN NOT NULL
+);
+
+INSERT INTO anime (name, type, url, year, stillRunning)
+VALUES 
+('JoJo Bizarre Adventure', 'Adventure', 'https://en.wikipedia.org/wiki/JoJo%27s_Bizarre_Adventure#/media/File:JoJo_no_Kimyou_na_Bouken_cover_-_vol1.jpg', 1987, TRUE),
+('Sailor Moon', 'Magical Girl', 'https://en.wikipedia.org/wiki/Sailor_Moon#/media/File:SMVolume1.jpg', 1991, FALSE),
+('Macross Frontier', 'Space Opera', 'https://en.wikipedia.org/wiki/File:MF_DVD_cover.jpg', 2007, FALSE),
+('Dragon Ball', 'Action', 'https://en.wikipedia.org/wiki/Dragon_Ball_(TV_series)#/media/File:Dragon_Ball_anime_logo.png', 1986, FALSE);
