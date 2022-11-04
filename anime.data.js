251938552d5399b2cd1e4e8463c178fc5639db96
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-03 21:45:03 -0700

    added data again

---
+++ anime.data.js
@@ -0,0 +1,45 @@
+const anime = [
+    {
+      id: '1',
+      name: 'JoJo Bizarre Adventure',
+      type: 'Adventure',
+      url: 'https://en.wikipedia.org/wiki/JoJo%27s_Bizarre_Adventure#/media/File:JoJo_no_Kimyou_na_Bouken_cover_-_vol1.jpg',
+      year: 1987,
+      stillRunning: 'yes',
+    },
+     {
+        id: '2',
+        name: 'Sailor Moon',
+        type: 'Magical Girl',
+        url: 'https://en.wikipedia.org/wiki/Sailor_Moon#/media/File:SMVolume1.jpg',
+        year: 1991,
+        stillRunning: 'no',
+     },
+     {
+        id: '3',
+        name: 'Sailor Moon',
+        type: 'Magical Girl',
+        url: 'https://en.wikipedia.org/wiki/Sailor_Moon#/media/File:SMVolume1.jpg',
+        year: 1991,
+        stillRunning: 'no',
+     },
+     {
+        id: '4',
+        name: 'Macross Frontier',
+        type: 'Space Opera',
+        url: 'https://en.wikipedia.org/wiki/File:MF_DVD_cover.jpg',
+        year: 2007,
+        stillRunning: 'no',
+     },
+     {
+        id: '5',
+        name: 'Dragon Ball',
+        type: 'Action',
+        url: 'https://en.wikipedia.org/wiki/Dragon_Ball_(TV_series)#/media/File:Dragon_Ball_anime_logo.png',
+        year: 1986 ,
+        stillRunning: 'no',
+     },
+    ];
+
+    module.exports = { anime };
+    
