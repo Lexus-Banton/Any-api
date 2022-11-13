85d50c91ae3e3b77ab7f0781525b008c6fe11ba4
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-12 20:50:07 -0800

    Almost forgot the test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { anime } = require('../lib/anime.data.js');
+
+describe('anime routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/anime should return a list of anime', async () => {
+    const res = await request(app).get('/anime');
+    const expected = anime.map((anime) => {
+      return { id: anime.id, name: anime.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
