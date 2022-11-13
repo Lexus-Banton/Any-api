fa9a3a1b47090a81f8621dac02e52c5ba29f4def
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-12 22:04:33 -0800

    fixed model and controllers test now passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,35 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-
-const { anime } = require('../lib/anime.data.js');
+const Anime = require('../lib/controllers/anime.js');
 
 describe('anime routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('/anime should return a list of anime', async () => {
     const res = await request(app).get('/anime');
-    const expected = anime.map((anime) => {
-      return { id: anime.id, name: anime.name };
-    });
-    expect(res.body).toEqual(expected);
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "JoJo Bizarre Adventure",
+        },
+        Object {
+          "id": "2",
+          "name": "Sailor Moon",
+        },
+        Object {
+          "id": "3",
+          "name": "Macross Frontier",
+        },
+        Object {
+          "id": "4",
+          "name": "Dragon Ball",
+        },
+      ]
+    `);
   });
 
   afterAll(() => {
